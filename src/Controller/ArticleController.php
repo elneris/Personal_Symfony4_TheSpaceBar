745f78d1f2fe300e7172b6bbad71855142dfcfc6
Elneris Dang <elneris.dang@gmail.com>
2019-05-30 22:48:46 +0200

    Route & Annotations

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('test');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response('test 2 ' . $slug );
+    }
+}
